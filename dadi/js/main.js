5a89ba94dd0385696410dbfdfe210f56a17235be
micheleprovolo <michele.provolo@gmail.com>
2019-10-21 23:12:51 +0200

    ex dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,19 @@
+//Gioco dei dadi, chi fa di più vince;
+
+var playerOne = Math.floor(Math.random() * 6) + 1;
+
+console.log(playerOne);
+
+
+var playerTwo = Math.floor(Math.random() * 6) + 1;
+
+console.log(playerTwo);
+
+if(playerOne > playerTwo) {
+ console.log("ha vinto il giocatore 1");
+ 
+} else if (playerOne == playerTwo){
+    console.log("risultato pari");
+} else {
+    console.log("ha vinto il giocatore 2");
+}
