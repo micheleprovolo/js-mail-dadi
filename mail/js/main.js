997d8c7c03f2fb4a5723a796d2b223c3ede4e766
micheleprovolo <michele.provolo@gmail.com>
2019-10-21 17:27:52 +0200

    esercizio mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,7 +1,21 @@
 //Chiedi all’utente la sua email
 
-var email = prompt("Scrivi la tua email")
+var askName = prompt("Scrivi il tuo nome");
 
-//controlla che sia nella lista di chi può accedere
-//e stampa un messaggio appropriato
+var lista = ["mario", "filippo", "lazio", "paco"];
+
+console.log(lista);
+
+var nome = "mario";
+
+//controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato
+
+for (var i = 0; i < lista.lenght; i++){
+
+    if(nome == lista[i]) {
+    
+    console.log("nome trovato", nome);
+    
+    }
+}
 
