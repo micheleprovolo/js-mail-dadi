cab5e1f4ec8c03217e118715df4b258639deb514
micheleprovolo <michele.provolo@gmail.com>
2019-10-21 22:46:19 +0200

    ex mail rivisto

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,21 +1,29 @@
 //Chiedi all’utente la sua email
 
-var askName = prompt("Scrivi il tuo nome");
+//var askEmail = prompt("Scrivi la tua mail");
 
 var lista = ["mario", "filippo", "lazio", "paco"];
 
 console.log(lista);
 
-var nome = "mario";
+var nome = "luigi";
+
+var found = false;
 
 //controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato
 
-for (var i = 0; i < lista.lenght; i++){
+for (var i = 0; i < lista.length; i++){
 
     if(nome == lista[i]) {
+    found = true;
+    console.log(found, "trovato!");
+    } 
     
-    console.log("nome trovato", nome);
-    
-    }
 }
 
+if(found == true) {
+    console.log("sei in lista");
+} else {
+    console.log("non sei in lista");
+    
+}
